4c5d769b28553c6bf84265855d7bc1cf6b645bf9
SSR <someshwar.sangaraju19@gmail.com>
2025-11-10 15:41:08 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,28 @@
-module "vpc" {
-    source = "git::https://github.com/SomeshwarSangaraju/terraform-aws-vpc.git?ref=main"
-    # vpc_cidr = "10.0.0.0/16"
-    # project_name = "roboshop"
-    # environment = "dev"
+# module "vpc" {
+
+#     source = "git::https://github.com/SomeshwarSangaraju/terraform-aws-vpc.git?ref=main"
+
+
+#     # VPC
+#     # vpc_cidr = var.vpc_cidr
+#     # project_name = var.project_name
+#     # environment = var.environment
+#     # vpc_tags = var.vpc_tags
+
+#     # # public subnets
+#     # public_subnet_cidrs = var.public_subnet_cidrs
+
+#     # # private subnets
+#     # private_subnet_cidrs = var.private_subnet_cidrs
+
+#     # # database subnets
+#     # database_subnet_cidrs = var.database_subnet_cidrs
+
+#     # is_peering_required = true
+# }
+
+module "vpc"{
+    source="git::https://github.com/SomeshwarSangaraju/terraform-aws-vpc.git"
 
     # VPC
     vpc_cidr = var.vpc_cidr
@@ -20,4 +40,5 @@ module "vpc" {
     database_subnet_cidrs = var.database_subnet_cidrs
 
     is_peering_required = true
+
 }
