6e8499673eac8de420a6624115bcda63f5cdf372
SSR <someshwar.sangaraju19@gmail.com>
2025-11-10 15:32:58 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-# module "aws_vpc"{
-    
-#     source="../terraform-aws-vpc"
-#     vpc_cidr=var.vpc_cidr
-#     instance_tentency=var.instance_tentency
-#     project_name =var.project
-#     environment = var.environment
-#     vpc_tags = var.vpc_tags
-
-#     #public subnet
-#     public_subnet_cidr = var.public_subnet_cidr
-
-#     #private subnet
-#     private_subnet_cidr = var.private_subnet_cidr
-
-#     #database subnet
-#     database_subnet_cidr = var.database_subnet_cidr
-
-#     is_peering_required = var.is_Peering_required
-# }
-
-
-
-
-
 
 module "vpc" {
     #source = "../terraform-aws-vpc"
@@ -48,7 +23,3 @@ module "vpc" {
 
     is_peering_required = true
 }
-
-# data "aws_availability_zones" "available" {
-#   state = "available"
-# }
